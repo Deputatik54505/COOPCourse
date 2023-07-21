37a8ed38e12a5b3014f2ba772b3ac3560f309579
ivan sannikov <i.sannikov@innopolis.university>
2023-07-21 19:33:09 +0300

    Add seller's orders page

--- Controller/src/main/java/org/example/Main.java
+++ Controller/src/main/java/org/example/Main.java
@@ -17,7 +17,7 @@ import java.util.Scanner;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/fxml/seller_acc_goods.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/fxml/seller_acc_orders.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 
         primaryStage.setTitle("Shop");
