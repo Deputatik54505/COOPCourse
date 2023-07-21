f149e2ca2691eb413337a20625668a784354de75
ivan sannikov <i.sannikov@innopolis.university>
2023-07-22 01:57:45 +0300

    Added product's and category's loads

--- Controller/src/main/java/org/example/Main.java
+++ Controller/src/main/java/org/example/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/fxml/seller_acc_settings.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/fxml/main_page.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 
         primaryStage.setTitle("Shop");
